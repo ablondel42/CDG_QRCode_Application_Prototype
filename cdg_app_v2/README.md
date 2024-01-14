4fb9ea433454f1e79bf998947e16e29f78e92614
ablondel42 <ablondel42@icloud.com>
2024-01-14 15:59:03 +0100

    clean start

---
+++ cdg_app_v2/README.md
@@ -0,0 +1,3 @@
+# cdg_app_v2
+
+A new Flutter project.
---
+++ cdg_app_v2/android/app/src/main/kotlin/com/example/cdg_app_v2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cdg_app_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
